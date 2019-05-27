8bea3e90322a0de63514fa6aae7368afdf09244c
Patrick Dugan <dugan.pat@gmail.com>
2019-05-27 08:00:58 -0700

    Nuked animation files

--- components/AsyncStorage.js
+++
@@ -1,8 +0,0 @@
-import { AsyncStorage } from 'react-native';
-
-export const saveState = async state => {
-    try {
-        const serializedState = JSON.stringify(state);
-        await AsyncStorage.setItem('state', serializedState);
-    } catch (error) {}
-};
