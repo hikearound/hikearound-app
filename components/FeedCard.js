4a5b6a238832491bc4cd954b801cfafe774b0655
Patrick Dugan <dugan.pat@gmail.com>
2019-05-28 14:24:11 -0700

    Dynamic linear gradient

--- components/FeedCard.js
+++ components/FeedCard.js
@@ -1,48 +1,67 @@
 import React from 'react';
 import styled from 'styled-components';
 import { LinearGradient } from 'expo';
+import { LayoutAnimation, Animated } from 'react-native';
 import colors from '../constants/Colors';
 import spacing from '../constants/Spacing';
 import fontSizes from '../constants/Fonts';
 
-const FeedCard = props => (
-    <Container>
-        <Cover>
-            <Image source={props.image} resizeMode='cover' />
-            <Title>{props.title}</Title>
-            <LinearGradient
-                colors={['rgba(0,0,0,0.6)', 'transparent']}
-                start={{ x: 1, y: 1 }}
-                end={{ x: 1, y: 0 }}
-                style={{
-                  position: 'absolute',
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  height: 100,
-              }}></LinearGradient>
-        </Cover>
-        <Content>
-            <ContentItem>
-                <MetaDataType>Distance</MetaDataType>
-                <MetaData>{props.distance}m</MetaData>
-            </ContentItem>
-            <ContentItem>
-                <MetaDataType>Elevation</MetaDataType>
-                <MetaData>{props.elevation}ft</MetaData>
-            </ContentItem>
-            <ContentItem>
-                <MetaDataType>Route</MetaDataType>
-                <MetaData>{props.route}</MetaData>
-            </ContentItem>
-        </Content>
-    </Container>
-);
+class FeedCard extends React.Component {
+    render() {
+        var CustomLayoutAnimation = {
+            duration: 50,
+            create: {
+                type: LayoutAnimation.Types.easeInEaseOut,
+                property: LayoutAnimation.Properties.opacity,
+            },
+            update: {
+                type: LayoutAnimation.Types.easeInEaseOut,
+            },
+        };
+        // LayoutAnimation.configureNext(CustomLayoutAnimation);
+        return (
+            <Container>
+                <Cover>
+                    <Image source={this.props.image} resizeMode='cover' />
+                    <Title>{this.props.title}</Title>
+                    <LinearGradient
+                        colors={[
+                            'rgba(0,0,0,0.6)',
+                            'transparent'
+                        ]}
+                        start={{ x: 1, y: 1 }}
+                        end={{ x: 1, y: 0 }}
+                        style={{
+                          position: 'absolute',
+                          left: 0,
+                          right: 0,
+                          bottom: 0,
+                          height: this.props.image.uri ? 75 : 0,
+                      }}></LinearGradient>
+                </Cover>
+                <Content>
+                    <ContentItem>
+                        <MetaDataType>Distance</MetaDataType>
+                        <MetaData>{this.props.distance}m</MetaData>
+                    </ContentItem>
+                    <ContentItem>
+                        <MetaDataType>Elevation</MetaDataType>
+                        <MetaData>{this.props.elevation}ft</MetaData>
+                    </ContentItem>
+                    <ContentItem>
+                        <MetaDataType>Route</MetaDataType>
+                        <MetaData>{this.props.route}</MetaData>
+                    </ContentItem>
+                </Content>
+            </Container>
+        );
+    }
+}
 
 export default FeedCard;
 
 const Container = styled.View`
-    background-color: #FFF;
+    background-color: #DEDEDE;
     width: 100%;
     border-radius: 6px;
     box-shadow: 0 4px 12px ${colors.transparentGray};
@@ -77,10 +96,11 @@ const Title = styled.Text`
 `;
 
 const Content = styled.View`
+    background-color: #FFF;
     flex-direction: row;
     align-items: center;
     position: relative;
-    margin: 10px 15px;
+    padding: 10px 15px;
 `;
 
 const ContentItem = styled.View`
