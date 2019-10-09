5b8dcb859f142ae6ebc527c85d6fc39c737e3f09
Patrick Dugan <dugan.pat@gmail.com>
2019-10-08 23:38:43 -0700

    Another try

--- Fire.js
+++ Fire.js
@@ -1,6 +1,3 @@
-import * as keys from './keys/Keys';
-
-const { firebaseKey, messagingSenderId, appId } = keys.default;
 const collectionName = 'hikes';
 const firebase = require('firebase');
 
@@ -9,13 +6,13 @@ require('firebase/firestore');
 class Fire {
     constructor() {
         firebase.initializeApp({
-            apiKey: firebaseKey,
+            apiKey: 'AIzaSyDzhRGewrBXqU6XPG5Bdl29JpPRPNtdilY',
             authDomain: 'hikearound-14dad.firebaseapp.com',
             databaseURL: 'https://hikearound-14dad.firebaseio.com',
             projectId: 'hikearound-14dad',
             storageBucket: 'hikearound-14dad.appspot.com',
-            messagingSenderId,
-            appId,
+            messagingSenderId: '175063732296',
+            appId: '1:175063732296:web:1f60ff2b03523972',
         });
     }
 
