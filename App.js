ca3ce5fd387343b9c71165f8f21e5c61d90e01fb
Patrick Dugan <dugan.pat@gmail.com>
2019-10-10 21:34:25 -0700

    Partial dark mode implementation

--- App.js
+++ App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { AppearanceProvider } from 'react-native-appearance';
 import AppNavigator from './navigator/AppNavigator';
 import reducer from './reducers/Index';
 
 const store = createStore(reducer);
+
 const App = () => (
     <Provider store={store}>
-        <AppearanceProvider>
-            <AppNavigator />
-        </AppearanceProvider>
+        <AppNavigator />
     </Provider>
 );
 
--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,4 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { createAppContainer } from 'react-navigation';
+import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import TabNavigator from './TabNavigator';
 
-export default createAppContainer(TabNavigator);
+const Navigation = createAppContainer(TabNavigator);
+
+const propTypes = {
+    darkMode: PropTypes.bool.isRequired,
+};
+
+function mapStateToProps(state) {
+    return {
+        darkMode: state.userReducer.darkMode,
+    };
+}
+
+function mapDispatchToProps() {
+    return {};
+}
+
+function App({ darkMode }) {
+    let theme = useColorScheme();
+
+    if (darkMode) {
+        theme = 'dark';
+    }
+
+    return (
+        <AppearanceProvider>
+            <Navigation theme={theme} />
+        </AppearanceProvider>
+    );
+}
+
+App.propTypes = propTypes;
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(App);
