1d9812f875930401f526dd677361e6117a3d6da8
Patrick Dugan <dugan.pat@gmail.com>
2019-10-17 17:57:49 -0700

    Nuked unused view

--- components/LoadingOverlay.js
+++ components/LoadingOverlay.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import { colors } from '../constants/Index';
 
 const propTypes = {
