c1c17cb160b8d5012387e3fcb0215e566beda4a2
Patrick Dugan <dugan.pat@gmail.com>
2019-11-07 13:46:06 -0800

    Sentry implementation base commit

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import * as Sentry from 'sentry-expo';
+import Constants from 'expo-constants';
 import AppNavigator from './navigator/AppNavigator';
 import reducer from './reducers/Index';
 
 const store = createStore(reducer);
+const dsn = 'https://5c8352d2c1f6437c9f678277bfc5528d@sentry.io/1811790';
 
 const App = () => (
     <Provider store={store}>
         <AppNavigator />
     </Provider>
 );
+
+Sentry.init({
+    dsn,
+    enableInExpoDevelopment: false,
+    debug: true,
+});
+
+Sentry.setRelease(Constants.manifest.revisionId);
 
 export default App;
