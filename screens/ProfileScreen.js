1a7508f8f3de7cf1053aa3383dacc94222e1dd49
Patrick Dugan <dugan.pat@gmail.com>
2020-01-28 13:16:01 -0800

    Resolves bad merge

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -9,6 +9,7 @@ import EditProfileModal from '../components/modals/EditProfileModal';
 import { getUserFavoriteHikes } from '../utils/User';
 import ProfileLoadingState from '../components/loading/Profile';
 import { initializeHikeData } from '../actions/Hike';
+import { timings } from '../constants/Index';
 
 const propTypes = {
     dispatchHikeData: PropTypes.func.isRequired,
@@ -32,10 +33,6 @@ function mapDispatchToProps(dispatch) {
 class ProfileScreen extends React.Component {
     static navigationOptions = ({ navigation }) => ({
         headerTitle: 'You',
-<<<<<<< HEAD
-        headerBackTitle: null,
-=======
->>>>>>> First pass
         headerRight: () => <Settings navigation={navigation} />,
     });
 
@@ -47,6 +44,12 @@ class ProfileScreen extends React.Component {
             maybeShowEmptyState: false,
             hikeData: [],
         };
+
+        this.loadingTimeout = setTimeout(() => {
+            this.setState({
+                shouldLoad: true,
+            });
+        }, timings.short);
     }
 
     async componentDidMount() {
@@ -91,13 +94,18 @@ class ProfileScreen extends React.Component {
     static contextType = ThemeContext;
 
     render() {
-        const { firstLoad, maybeShowEmptyState, hikeData } = this.state;
+        const {
+            firstLoad,
+            maybeShowEmptyState,
+            hikeData,
+            shouldLoad,
+        } = this.state;
 
         const theme = themes[this.context];
 
         return (
             <RootView theme={theme}>
-                {firstLoad && <ProfileLoadingState />}
+                {firstLoad && shouldLoad && <ProfileLoadingState />}
                 {!firstLoad && (
                     <>
                         <ProfileHeader />
