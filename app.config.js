652e049a6cfe2b25a5bf3055c25b87167712e525
Patrick Dugan <dugan.pat@gmail.com>
2020-06-18 14:26:49 -0400

    Config nits

--- app.config.js
+++ app.config.js
@@ -1,15 +1,27 @@
 import 'dotenv/config';
 
 export default {
+    hooks: {
+        postPublish: [
+            {
+                file: 'sentry-expo/upload-sourcemaps',
+                config: {
+                    organization: 'hikearound',
+                    project: 'hikearound-app',
+                    authToken: process.env.SENTRY_AUTH_TOKEN,
+                },
+            },
+        ],
+    },
     extra: {
         firebase: {
+            appId: '1:175063732296:web:1f60ff2b03523972',
             apiKey: process.env.FIREBASE_KEY,
             authDomain: 'hikearound-14dad.firebaseapp.com',
             databaseURL: 'https://hikearound-14dad.firebaseio.com',
             projectId: 'hikearound-14dad',
             storageBucket: 'hikearound-14dad.appspot.com',
             messagingSenderId: '175063732296',
-            appId: '1:175063732296:web:1f60ff2b03523972',
         },
         sentry: {
             dsn: 'https://5c8352d2c1f6437c9f678277bfc5528d@sentry.io/1811790',
