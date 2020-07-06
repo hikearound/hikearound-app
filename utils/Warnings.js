f3b8c235fb151b3133339de03cec76a22ab9b5ee
Patrick Dugan <dugan.pat@gmail.com>
2020-07-05 20:49:29 -0400

    Style nit

--- utils/Warnings.js
+++ utils/Warnings.js
@@ -1,11 +1,7 @@
 import { YellowBox } from 'react-native';
 
 export function ignoreWarnings() {
-    /* eslint-disable-next-line */
-    YellowBox.ignoreWarnings(
-        ['getNode', 'useNativeDriver'],
-
-    );
+    YellowBox.ignoreWarnings(['getNode', 'useNativeDriver']);
 }
 
 export default ignoreWarnings;
