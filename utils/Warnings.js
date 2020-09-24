d4b7bf51c7b311a56196895709ec8ff896ddd492
Patrick Dugan <dugan.pat@gmail.com>
2020-09-24 08:14:34 -0400

    Experimental reanimated package

--- utils/Warnings.js
+++ utils/Warnings.js
@@ -2,6 +2,7 @@ import { LogBox } from 'react-native';
 
 export function ignoreWarnings() {
     LogBox.ignoreLogs([
+        'Calling `getNode()` on the ref of an Animated component is no longer necessary.',
         'Unhandled promise rejection: Error: internal',
         'Warning: Cannot update a component from inside the function body of a different component',
         'Error: Native splash screen is already hidden',
