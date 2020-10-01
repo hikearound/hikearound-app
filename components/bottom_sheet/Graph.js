59b08380b35213d909474a82fa96d94ce2517f38
Patrick Dugan <dugan.pat@gmail.com>
2020-09-30 21:32:51 -0400

    Lock it

--- components/bottom_sheet/Graph.js
+++ components/bottom_sheet/Graph.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import BottomSheet from 'reanimated-bottom-sheet';
+import {
+    ChartDot,
+    ChartPath,
+    ChartPathProvider,
+    ChartXLabel,
+    ChartYLabel,
+    monotoneCubicInterpolation,
+} from '@rainbow-me/animated-charts';
+import { Dimensions } from 'react-native';
 import { withTheme } from '../../utils/Themes';
-import { spacing, bottomSheet } from '../../constants/Index';
+import { bottomSheet } from '../../constants/Index';
 import { withNavigation } from '../../utils/Navigation';
 import SheetHeader from './Header';
 
@@ -14,34 +23,93 @@ const propTypes = {
 
 const defaultProps = {};
 
-class GraphSheet extends React.Component {
-    renderContent = () => {
-        const { elevationArray } = this.props;
-        console.log(elevationArray);
-        return <Body />;
+export const { width } = Dimensions.get('window');
+
+function GraphSheet({ sheetRef, elevationArray }) {
+    const [selectedElevation, setSelectedElevation] = useState(0);
+    const [selectedDistance, setSelectedDistance] = useState(0);
+
+    const formatElevation = (value) => {
+        'worklet';
+
+        if (value === '') {
+            return `${selectedElevation} ft`;
+        }
+
+        const elevation = value.toLocaleString('en-US', {
+            maximumFractionDigits: 2,
+        });
+
+        setSelectedElevation(value);
+        return `${elevation} ft`;
     };
 
-    renderHeader = () => {
+    const formatDistance = (value) => {
+        'worklet';
+
+        if (value === '') {
+            return `${selectedDistance} miles`;
+        }
+
+        setSelectedDistance(value);
+        return `${value} miles`;
+    };
+
+    const renderContent = () => {
+        const points = monotoneCubicInterpolation({
+            data: elevationArray,
+            range: 1000,
+        });
+
+        return (
+            <Body>
+                <ChartPathProvider
+                    data={{ points, smoothingStrategy: 'bezier' }}
+                >
+                    <ChartPath
+                        height={width / 2}
+                        stroke='black'
+                        width={width}
+                        strokeWidth={2}
+                        selectedStrokeWidth={2}
+                    />
+                    <ChartYLabel
+                        format={formatElevation}
+                        style={{
+                            color: 'green',
+                            margin: 4,
+                        }}
+                    />
+                    <ChartXLabel
+                        format={formatDistance}
+                        style={{
+                            color: 'red',
+                            margin: 4,
+                        }}
+                    />
+                    <ChartDot style={{ backgroundColor: 'blue' }} />
+                </ChartPathProvider>
+            </Body>
+        );
+    };
+
+    const renderHeader = () => {
         return <SheetHeader />;
     };
 
-    render() {
-        const { sheetRef } = this.props;
-
-        return (
-            <BottomSheet
-                snapPoints={[
-                    bottomSheet.chart.expanded,
-                    bottomSheet.chart.expanded,
-                    bottomSheet.chart.collapsed,
-                ]}
-                renderContent={this.renderContent}
-                renderHeader={this.renderHeader}
-                enabledInnerScrolling={false}
-                ref={sheetRef}
-            />
-        );
-    }
+    return (
+        <BottomSheet
+            snapPoints={[
+                bottomSheet.chart.expanded,
+                bottomSheet.chart.expanded,
+                bottomSheet.chart.collapsed,
+            ]}
+            renderContent={renderContent}
+            renderHeader={renderHeader}
+            enabledInnerScrolling={false}
+            ref={sheetRef}
+        />
+    );
 }
 
 GraphSheet.propTypes = propTypes;
@@ -51,6 +119,5 @@ export default withNavigation(withTheme(GraphSheet));
 
 const Body = styled.View`
     height: 500px;
-    padding: ${spacing.small}px;
     background-color: ${(props) => props.theme.sheetBackground};
 `;
